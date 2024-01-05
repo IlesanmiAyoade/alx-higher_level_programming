33d0f487a2b40554f4538f5407701a0c91131ab4
IlesanmiAyoade <ilesanmibenjaminay@gmail.com>
2024-01-05 18:41:03 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const argument = process.argv;
-if (argument[2] === undefined || isNaN(argument[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
-}
-else {
-  const x = Number(argument[2]);
+} else {
+  const x = Number(process.argv[2]);
   let i = 0;
   while (i < x) {
     console.log('C is fun');
